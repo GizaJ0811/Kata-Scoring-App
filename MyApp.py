873c2da7242edd7e20b06240ad4d9f7c92497c99
GizaJ0811 <gizajirndhara@gmail.com>
2022-04-10 08:37:16 +0700

    UI detail added

--- MyApp.py
+++ MyApp.py
@@ -1,20 +1,54 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
+from kivymd.uix.screen import MDScreen
 
 KV = """
-BoxLayout:
-    size_hint: root.size
-    MDRectangleFlatButton:
-        text: "tell a secret"
-        pos_hint: {"center_x": .5, "center_y": .5}
-"""
+<ScoreInput@MDTextField>
+    mode: "fill"
+    icon_right: 'scoreboard'
+    size_hint_x: .6
+    pos_hint: {'center_x': .5}
 
+<Main>
+    Screen:
+        MDCard:
+            size_hint: .7, 0.7
+            pos_hint: {'center_x': 0.5,'center_y': 0.5}
+            orientation: 'vertical'
+            MDLabel:
+                text: "Score"
+                halign: 'center'
+                font_size: 25
+                bold: True
+                size_hint_y: None
+                y: self.texture_size[1]
+            MDBoxLayout:
+                spacing: 10
+                padding: 15
+                orientation: 'vertical'
+                size_hint: .9, .9
+                pos_hint: {'center_x': 0.5,'center_y': 0.5}
+                ScoreInput:
+                    hint_text: 'Score'
+                ScoreInput:
+                    hint_text: 'Score2'
+                MDFillRoundFlatButton: 
+                    id: btn
+                    pos_hint: {'center_x': .5,'center_y': .5}
+                    text: "Submit"
+                    color: 'blue'
+        
+"""
+class Main(MDScreen):
+    def __init__(self) -> None:
+        super().__init__()
+        
 class MyApp(MDApp):
     def build(self):
         self.title = "Scoring App"
-        self.theme_cls.primary_palette = "LightGreen"
+        self.theme_cls.primary_palette = 'Indigo'
         self.theme_cls.theme_style = "Dark"
-        return Builder.load_string(KV)
-
+        return Main()
+Builder.load_string(KV)
 if __name__ == '__main__':
     MyApp().run()
